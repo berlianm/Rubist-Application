e62c00ba0459c2bf460b14640b62959e9b2daa6b
Gutri Rahmad Zuwa <gutri363696@gmail.com>
2023-06-09 13:54:04 +0700

    AddPost

--- Mobile APP/C23-PS480_Rubist/app/src/main/java/com/C23PS480/Rubist/Fragment/Community.kt
+++ Mobile APP/C23-PS480_Rubist/app/src/main/java/com/C23PS480/Rubist/Fragment/Community.kt
@@ -6,16 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
-import com.C23PS480.Rubist.AddPost.AddPostActivity
+import com.C23PS480.Rubist.API.Response.ListPost
+//import com.C23PS480.Rubist.AddPost.AddPostActivity
 import com.C23PS480.Rubist.MainViewModel
 import com.C23PS480.Rubist.R
+import com.C23PS480.Rubist.databinding.FragmentCommunityBinding
+
 //import com.C23PS480.Rubist.RecycleViewAdapter
 
 
 class Community : Fragment() {
+    private var _binding: FragmentCommunityBinding? = null
+    private val binding get()= _binding!!
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var progressBar: ProgressBar
+
+    private val post: MutableList<ListPost> = mutableListOf()
+
     private var layoutManager: RecyclerView.LayoutManager? = null
 //    private var adapter: RecyclerView.Adapter<RecycleViewAdapter.ViewHolder>? = null
     private lateinit var mainViewModel : MainViewModel
@@ -27,7 +39,7 @@ class Community : Fragment() {
         val BtnAddPost: TextView = view.findViewById(R.id.tv_addPost)
        BtnAddPost.setOnClickListener {
             // Pindah ke activity lain
-            val intent = Intent(activity, AddPostActivity::class.java)
+            val intent = Intent(activity, AddPost::class.java)
             startActivity(intent)
 
         }
@@ -36,11 +48,25 @@ class Community : Fragment() {
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community, container, false)
+        _binding = FragmentCommunityBinding.inflate(inflater, container, false)
+
+        val root :View = binding.root
+
+        recyclerView = binding.rvPost
+        progressBar = binding.progressBar
+
+//        setupRecyclerView()
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
